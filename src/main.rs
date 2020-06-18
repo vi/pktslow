22f61f80579418623300d1879f2a26406e67a4e0
Vitaly _Vi Shukela <vi0oss@gmail.com>
2020-06-18 19:31:00 +0300

    Conditional delay

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,27 @@
 use tun_tap::{Iface,Mode};
 
 use crossbeam_utils::thread::scope;
+use crossbeam_channel::{unbounded,Sender,Receiver};
+use std::time::{Duration,Instant};
 
-fn copy(if1: &Iface, if2: &Iface) {
+fn copy(ifr: &Iface, ifw: &Iface, del:Sender<(Instant, Vec<u8>)>) {
     let mut buf = [0u8; 4096];
-    while let Ok(l) = if1.recv(&mut buf[..]) {
+    let mut mood = false;
+    while let Ok(l) = ifr.recv(&mut buf[..]) {
         let buf : &[u8] = & buf[0..l];
-        let _ = if2.send(buf);
+        if mood {
+            let _ = ifw.send(buf);
+        } else {
+            let _ = del.send((Instant::now() + Duration::from_secs(1), buf.to_vec()));
+        }
+        mood = ! mood;
+    }
+}
+
+fn delayline(if_: &Iface, r: Receiver<(Instant, Vec<u8>)>) {
+    while let Ok((t, b)) = r.recv() {
+        std::thread::sleep(t.saturating_duration_since(Instant::now()));
+        let _ = if_.send(&b[..]);
     }
 }
 
@@ -15,11 +30,22 @@ fn main() -> anyhow::Result<()> {
     let if2 = Iface:: without_packet_info("tun2", Mode::Tun)?;
 
     let _ = scope(|s|  {
-        let _h1 = s.spawn(|_| {
-            copy(&if1, &if2);
+        let (del1s, del1r) = unbounded();
+        let (del2s, del2r)  = unbounded();
+
+        let _h = s.spawn(|_| {
+            copy(&if1, &if2, del1s);
         });
-        let _h2 = s.spawn(|_| {
-            copy(&if2, &if1);
+        let _h = s.spawn(|_| {
+            delayline(&if2, del1r);
+        });
+        
+
+        let _h = s.spawn(|_| {
+            copy(&if2, &if1, del2s);
+        });
+        let _h = s.spawn(|_| {
+            delayline(&if1, del2r);
         });
     });
 
